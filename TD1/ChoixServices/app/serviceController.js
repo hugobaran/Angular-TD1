6a583f6041249bdfad44bddf4e4c9d2b0313e503
hugobaran <hugo.baran@me.com>
2017-01-31 15:55:31 +0100

    Calcul du total

--- TD1/ChoixServices/app/serviceController.js
+++ TD1/ChoixServices/app/serviceController.js
@@ -1,5 +1,12 @@
 serviceApp.controller('ServiceController', function($http){
 	var self = this;
+	var self = this;
+	this.cpt = 1;
+	this.totals = 300;
+	this.promoCoche = false;
+	this.promoError = false;
+	this.totalAvecRemise = 0;
+	this.remise = 0;
 
     this.services = [
 	    {
@@ -20,4 +27,31 @@ serviceApp.controller('ServiceController', function($http){
 	        "active":false
 	    }
 	];
+	
+	 self.total = function(){
+    	var somme = 0;
+    	angular.forEach(self.services, function(service, key){
+    		if(service.active)
+    			somme+=service.price;
+    	});
+    	self.totals = somme;
+};
+	
+	self.toggleActive = function($item){
+    	// Test si déjà actif
+    	if(!self.services[$item].active)
+    		self.services[$item].active = true;
+    	else
+    		self.services[$item].active = false;
+
+		this.total();
+		
+    	// Compte le nombre de services actifs
+    	var cpt = 0;
+    	angular.forEach(this.services, function(value, key){
+    		if(value.active)
+    			cpt++;
+    	});
+    	this.cpt = cpt;
+};
 });
