46670b9cdda170f28ef4752a0ec99faf53c2d129
hugobaran <hugo.baran@me.com>
2017-02-14 12:13:22 +0100

    Directive Ajout contact formulaire

--- TD2/GestionContacts/app/gestionController.js
+++ TD2/GestionContacts/app/gestionController.js
@@ -3,25 +3,39 @@ gestionApp.controller('gestionController', function(){
 	this.contact;
 	this.tmpContact;
 	this.operation;
-	this.edit;
+	this.edit = false;
+	this.filtre = ""
+	this.operation = "";
+
 
 	//Tableau des contacts existants
 	self.contacts = [
 		{
             "nom": "ZUCKERBERG",
             "prenom": "Mark",
-            "mail": "mark@facebook.com"
+            "mail": "mark@facebook.com",
+            "deleted": false
         },
         {
             "nom": "GATES",
             "prenom": "Bill",
-            "mail": "bill@microsoft.com"
+            "mail": "bill@microsoft.com",
+            "deleted": false
         },
         {
             "nom": "JOBS",
             "prenom": "Steeve",
-            "mail": "steeve@apple.com"
+            "mail": "steeve@apple.com",
+            "deleted": false
         }
+    ];
+
+    self.tmpContact = [
+    	{
+    		"nom" : "",
+    		"prenom" : "",
+    		"mail" : "" 
+    	}
     ];
 
 	self.toUpdate = function(contact){
@@ -32,12 +46,23 @@ gestionApp.controller('gestionController', function(){
     };
 
     self.add = function(){
+
     };
 
     self.update = function(){
     };
 
     self.delete = function(contact){
+    	alert(contact)
+    	i = self.contacts.indexOf(contact);
+    	alert('i')
+        //self.contacts[i].deleted = true;
     };
 
+});
+
+gestionApp.directive('frmContactElem', function() {
+    return {
+        templateUrl: 'app/frmContacts.html'
+    };
 });
