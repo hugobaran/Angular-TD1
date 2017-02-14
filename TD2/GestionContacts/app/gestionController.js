fe244a52cc3c79edeacd078bc2b43c5d3adba99e
hugobaran <hugo.baran@me.com>
2017-02-14 11:20:43 +0100

    Mise en page de l'app

--- TD2/GestionContacts/app/gestionController.js
+++ TD2/GestionContacts/app/gestionController.js
@@ -1,4 +1,43 @@
 gestionApp.controller('gestionController', function(){
 	var self = this;
+	this.contact;
+	this.tmpContact;
+	this.operation;
+	this.edit;
+
+	//Tableau des contacts existants
+	self.contacts = [
+		{
+            "nom": "ZUCKERBERG",
+            "prenom": "Mark",
+            "mail": "mark@facebook.com"
+        },
+        {
+            "nom": "GATES",
+            "prenom": "Bill",
+            "mail": "bill@microsoft.com"
+        },
+        {
+            "nom": "JOBS",
+            "prenom": "Steeve",
+            "mail": "steeve@apple.com"
+        }
+    ];
+
+	self.toUpdate = function(contact){
+    };
+
+    self.toAdd = function(){
+    	self.edit=true;
+    };
+
+    self.add = function(){
+    };
+
+    self.update = function(){
+    };
+
+    self.delete = function(contact){
+    };
 
 });
