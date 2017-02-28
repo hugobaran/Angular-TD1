43913ef69c341e6b20939446f0dc74127e702b26
hugobaran <hugo.baran@me.com>
2017-02-28 14:22:24 +0100

    récupération devises + interchanger 2 devises ok

--- TD3/ConvertisseurDevise/app/deviseController.js
+++ TD3/ConvertisseurDevise/app/deviseController.js
@@ -1,9 +1,14 @@
 currencyApp.controller('currencyController', ['$http', function($http) {	
 	var self = this;
+	this.currencies;
+    this.what = 1;
+    this.from = "EUR";
+    this.to = "USD";
+    this.result;
+    this.historique = false;
 
 
 	//initialisation de la monnaie 
-	
 	$http.get('app/data/currencymap.json').
     then(function(response) {
         self.currencies = response.data;
@@ -12,6 +17,9 @@ currencyApp.controller('currencyController', ['$http', function($http) {
         console.log("Erreur avec le statut Http : "+response.status);
     });
 
-
-
+    this.swap = function(){
+        var temp = self.from;
+        self.from = self.to;
+        self.to = temp;
+    }
 }]);
